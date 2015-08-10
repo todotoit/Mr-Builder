1fe91b73793a9b4a551951c13551db97a1ed1a15
abusedmedia <ff@todo.to.it>
2015-08-10 09:47:39 +0200

    added ngannotate and ngtemplates

--- app/js/app.js
+++ app/js/app.js
@@ -3,3 +3,18 @@
 
   console.log('hello, world');
 })(); 
+
+
+var app = angular.module('app', []);
+
+app.controller('main', function($scope, $http){
+	console.log('main');
+});
+
+app.service('srv', function($rootScope){
+	console.log('srv');
+});
+
+app.factory('fack', function($http){
+	console.log('fack');
+});
