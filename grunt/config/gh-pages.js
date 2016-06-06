db2aa3dbcb8411e152d8233c42854724d79deb7a
abusedmedia <ff@todo.to.it>
2016-06-06 22:16:30 +0200

    added grunt gh pages task

---
+++ grunt/config/gh-pages.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = function (grunt, options) {
+
+  // this is the grunt configuration object
+  return {
+
+    options: {
+      base: options.folders.dist,
+      message: 'Git-Pages Deploy'
+    },
+    'gh-pages': {
+      options: {
+      },
+      src: ['**']
+    },
+    'other-staging': {
+      options: {
+        branch: 'gh-pages',
+        repo: 'https://example.com/other/repo.git'
+      },
+      src: ['**']
+    }
+
+
+  };
+
+};
