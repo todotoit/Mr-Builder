873403ccb6c4efe20fb0fd5af83afa5d3d9c180d
abusedmedia <ff@todo.to.it>
2016-06-24 10:15:18 +0200

    added index for version release

--- grunt/tasks/create_index.js
+++ grunt/tasks/create_index.js
@@ -10,15 +10,17 @@ module.exports = function () {
     var folders = grunt.config('folders');
 	var version = grunt.config('version')
 
+	var str = '<html><head><title>...</title><meta http-equiv="refresh" content="0;URL=\''+version+'/\'" /></head><body></body></html>'
+
+	/*
 	var dir = path.join(folders.app, '')
-
 	var list = fs.readdirSync(dir)
-
-	var str = '<html><head></head><body><ul>'
+	var str = '<html><head><title>...</title></head><body><ul>'
 	list.forEach(function(d, i){
 		str += '<li><a href="'+d+'/">'+d+'</a></li>'
 	})
 	str += '</ul></body></html>'
+	*/
 
 	grunt.file.write(path.join(folders.app, 'index.html'), str);
 
